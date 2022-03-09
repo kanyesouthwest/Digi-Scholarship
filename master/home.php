e9a8070a22f0cc0e6a5e0f6431e140b6f1d05a6f
digstm0126 <78323032+digstm0126@users.noreply.github.com>
2022-03-10 09:50:07 +1300

    did some set up for the first page

---
+++ master/home.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>home</title>
+  </head>
+  <body>
+
+    <!-- the coteaner to hold every thing -->
+    <div class="container-fluid">
+
+      <div class="row">
+
+        <div class="col-6 border-10">
+          <div class="row">
+          <div class="col-6 bg-success">
+            <p> image </p>
+          </div>
+          <div class="col-6 bg-primary">
+            <p> luch </p>
+          </div>
+        </div>
+        </div>
+
+        <div class="col-6">
+          <div class="row">
+          <div class="col-6 bg-success">
+            <p> image </p>
+          </div>
+          <div class="col-6 bg-primary">
+            <p> luch </p>
+          </div>
+        </div>
+        </div>
+
+        <div class="col-6">
+          <div class="row">
+          <div class="col-6 bg-success">
+            <p> image </p>
+          </div>
+          <div class="col-6 bg-primary">
+            <p> luch </p>
+          </div>
+        </div>
+        </div>
+
+        <div class="col-6">
+          <div class="row">
+          <div class="col-6 bg-success">
+            <p> image </p>
+          </div>
+          <div class="col-6 bg-primary">
+            <p> luch </p>
+          </div>
+        </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </body>
+</html>
