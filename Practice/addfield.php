199c3b782209d086c36cc9b6aebd4c7935ad4bad
kanyesouthwest <ath7073@stacmail.net>
2022-03-09 16:00:53 +1300

    Practice code updated

--- Practice/addfield.php
+++ Practice/addfield.php
@@ -15,7 +15,7 @@
 
             // Declare variable using ID from form
             $student_ID = mysqli_real_escape_string($dbconnect, $_POST['student_ID']);
-
+            
 
             // Select all information dependiong on ID
             $student_sql = "SELECT * FROM student_details WHERE student_ID = $student_ID";
@@ -25,13 +25,16 @@
             // Confirm if sudent has/has not signed out
             $search_sql = "SELECT * FROM student_log WHERE student_ID = $student_ID";
             $search_qry = mysqli_query($dbconnect,$search_sql);
+            $search_aa = mysqli_fetch_assoc($search_qry);  
 
+            if (mysqli_num_rows($search_qry) != 0 ) {
+                $student_status = $search_aa['in_school'];
+            }
 
-
+            // if (mysqli_num_rows($search_qry) == 0 ) {
 
             // Check if student has not signed out
-            if (mysqli_num_rows($search_qry) == 0) {
-
+            if ((mysqli_num_rows($search_qry) == 0 ) or ($student_status == 0)) {
 
                 // Declare variables using form * student_details table
                 $first_name = $student_aa['first_name'];
@@ -41,7 +44,7 @@
 
 
                 // Inserts data into log table
-                $sign_out_sql = "INSERT INTO student_log (student_ID, first_name, last_name, reason, time_out) VALUES ('$student_ID','$first_name','$last_name','$reason', (NOW()))";
+                $sign_out_sql = "INSERT INTO student_log (student_ID, first_name, last_name, reason, time_out') VALUES ('$student_ID','$first_name','$last_name','$reason', (NOW()) ";
                 $sign_out_qry = mysqli_query($dbconnect, $sign_out_sql);
         ?>
 
