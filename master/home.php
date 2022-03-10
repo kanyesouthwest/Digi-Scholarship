a3d8652916ac18072d6f6d05c02328dbfeaa8fc6
digstm0126 <78323032+digstm0126@users.noreply.github.com>
2022-03-11 12:34:48 +1300

    got the basic work
    sorted out some

--- master/home.php
+++ master/home.php
@@ -9,55 +9,66 @@
     <!-- the coteaner to hold every thing -->
     <div class="container-fluid">
 
-      <div class="row">
 
-        <div class="col-6 border-10">
-          <div class="row">
-          <div class="col-6 bg-success">
-            <p> image </p>
-          </div>
-          <div class="col-6 bg-primary">
-            <p> luch </p>
-          </div>
-        </div>
-        </div>
 
-        <div class="col-6">
-          <div class="row">
-          <div class="col-6 bg-success">
-            <p> image </p>
-          </div>
-          <div class="col-6 bg-primary">
-            <p> luch </p>
-          </div>
-        </div>
-        </div>
+    <div class="row">
 
-        <div class="col-6">
-          <div class="row">
-          <div class="col-6 bg-success">
-            <p> image </p>
+      <div class="col-6 bg-danger">
+        <div class="row">
+          <div class="col-6">
+            <div class="row bg-primary">
+              <div class="col-3">
+                Image
+              </div>
+              <div class="col-9">
+                Lunch
+              </div>
+            </div>
           </div>
-          <div class="col-6 bg-primary">
-            <p> luch </p>
+          <div class="col-6">
+            <div class="row bg-primary">
+              <div class="col-3">
+                Image
+              </div>
+              <div class="col-9">
+                Lunch
+              </div>
+            </div>
           </div>
-        </div>
+          </div>
         </div>
 
-        <div class="col-6">
-          <div class="row">
-          <div class="col-6 bg-success">
-            <p> image </p>
-          </div>
-          <div class="col-6 bg-primary">
-            <p> luch </p>
-          </div>
-        </div>
+
+
+
+
+
+
+        
+
+      <div class="col-6 bg-primary">
+
+        <div class="col-3 bg-success">
+          <p>name last name</p>
         </div>
 
+        <div class="col-3 bg-success">
+          <p>not you?</p>
+          <p>sight out</p>
+        </div>
+
+        <div class="col-3 bg-success">
+          <p>select</p>
+        </div>
       </div>
 
     </div>
+
+
+
+
+  </div>
+
 
   </body>
 </html>
