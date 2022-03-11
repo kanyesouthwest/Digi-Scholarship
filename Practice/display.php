5da6d0b372a7905cab6b215cdb4f491f0c640caf
kanyesouthwest <60488904+kanyesouthwest@users.noreply.github.com>
2022-03-12 12:45:22 +1300

    Echoing historical data

--- Practice/display.php
+++ Practice/display.php
@@ -39,25 +39,34 @@
 
             <?php
 
-            $student_old_sql = "SELECT * FROM student_transactions";
+
+
+            $select_ID_sql = "SELECT * FROM student_transactions";
+            $select_ID_qry = mysqli_query($dbconnect, $select_ID_sql);
+            $select_ID_aa = mysqli_fetch_assoc($select_ID_qry);  
+
+            
+            $fetch_group_ID = $select_ID_aa['group_ID'];
+
+
+            $student_old_sql = "SELECT * FROM student_transactions WHERE group_ID = $fetch_group_ID";
             $student_old_qry = mysqli_query($dbconnect, $student_old_sql);
             $student_old_aa = mysqli_fetch_assoc($student_old_qry);  
 
             do {
-
+                
                 echo "<p>Student ID: " . $student_old_aa['student_ID'] . "</p>";
                 echo "<p>First Name: " . $student_old_aa['first_name'] . "</p>";
                 echo "<p>Last Name: " . $student_old_aa['last_name'] . "</p>";
                 echo "<p>Reason: " . $student_old_aa['reason'] . "</p>";
                 echo "<p>Time out: " . $student_old_aa['time_out'] . "</p>";
                 echo "<p>Time in: " . $student_old_aa['time_in'] . "</p>";
+                ?> <br> <?php
+
+            } while ($student_old_aa = mysqli_fetch_assoc($student_old_qry));
 
 
-
-            } while ($student_aa = mysqli_fetch_assoc($student_qry));
-
-
-            $group_ID_sql = "SELECT group_ID FROM student_transactions ORDER BY group_ID  DESC limit 1";
+            $group_ID_sql = "SELECT group_ID FROM student_transactions ORDER BY group_ID  DESC LIMIT 1";
             $group_ID_qry = mysqli_query($dbconnect, $group_ID_sql);
             $group_ID_aa = mysqli_fetch_assoc($group_ID_qry);  
 
@@ -72,3 +81,7 @@
         ?>
     </body>
 </html>
+
+
+
+
