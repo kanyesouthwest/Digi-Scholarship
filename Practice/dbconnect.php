835117202e47063ea95f00d3154921fc75ef86ba
kanyesouthwest <ath7073@stacmail.net>
2022-05-04 09:50:48 +1200

    Added teacher only section to master

--- Practice/dbconnect.php
+++ Practice/dbconnect.php
@@ -1,4 +1,4 @@
 <!-- to access the db -->
 <?php
-$dbconnect = mysqli_connect("localhost", "root", "", "sign out system_db");
+$dbconnect = mysqli_connect("localhost", "root", "", "sign_out_system_db");
 ?>
