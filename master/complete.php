3d97cf0a0de7eb3314215c3f1873da233ec34c61
digstm0126 <78323032+digstm0126@users.noreply.github.com>
2022-05-20 13:38:33 +1200

    added stuff
    added fonts but not working so testing on windows device

--- master/complete.php
+++ master/complete.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8">
     <title>Sucess</title>
   </head>
-  <body>
+  <body style="background-color: rgb(0,65,122);">
     <?php
     session_start();
-    
+
       if(!isset($_POST['reason'])) {
           header("Location: home.php");
 
@@ -17,17 +17,17 @@
         // Select all information depnding on ID
         $student_sql = "SELECT * FROM student_details WHERE student_ID = $student_ID";
         $student_qry = mysqli_query($dbconnect, $student_sql);
-        $student_aa = mysqli_fetch_assoc($student_qry);  
+        $student_aa = mysqli_fetch_assoc($student_qry);
 
         // Confirm if sudent has/has not signed out
         $search_sql = "SELECT * FROM student_log WHERE student_ID = $student_ID";
         $search_qry = mysqli_query($dbconnect,$search_sql);
-        $search_aa = mysqli_fetch_assoc($search_qry);  
+        $search_aa = mysqli_fetch_assoc($search_qry);
 
         // Selects latest time specific student signed out
         $order_sql = "SELECT * FROM student_log WHERE student_ID = $student_ID ORDER BY time_out DESC LIMIT 1";
         $order_qry = mysqli_query($dbconnect,$order_sql);
-        $order_aa = mysqli_fetch_assoc($order_qry);  
+        $order_aa = mysqli_fetch_assoc($order_qry);
 
         // If student has signed out before
         if (mysqli_num_rows($search_qry) != 0 ) {
@@ -35,8 +35,8 @@
             // Varible if they are in school or not
             // 1 = IN
             // 2 = OUT
-            $student_status = $order_aa['in_school'];   
-            
+            $student_status = $order_aa['in_school'];
+
             // Declare variables using form & student_details table
             $first_name = $student_aa['first_name'];
             $last_name = $student_aa['last_name'];
@@ -60,7 +60,7 @@
 
             $group_ID_sql = "SELECT group_ID FROM student_transactions ORDER BY group_ID  DESC limit 1";
             $group_ID_qry = mysqli_query($dbconnect, $group_ID_sql);
-            $group_ID_aa = mysqli_fetch_assoc($group_ID_qry);  
+            $group_ID_aa = mysqli_fetch_assoc($group_ID_qry);
 
             $group_ID_unique = $group_ID_aa['group_ID'];
             $group_ID_unique = $group_ID_unique + 1;
@@ -71,9 +71,17 @@
 
             ?>
 
-            <h2> You have signed out</h2>
-            <p>Redirect in 3</p>
-            <?php 
+
+            <div class="container d-flex align-items-center justify-content-center vstack text-light">
+
+                  <h1 class="display-1">you have signed out</h1>
+
+                  <p>you will be redorected in 3 secods</p>
+
+            </div>
+
+
+            <?php
             header('Refresh:3 ; URL=index.php?page=home');
 
             } else {
@@ -84,7 +92,7 @@
 
         //     $get_group_sql = "SELECT group_ID FROM student_transactions WHERE student_ID = $student_ID ORDER BY group_ID DESC LIMIT 1";
         //     $get_group_qry = mysqli_query($dbconnect, $get_group_sql);
-        //     $get_group_aa = mysqli_fetch_assoc($get_group_qry);  
+        //     $get_group_aa = mysqli_fetch_assoc($get_group_qry);
 
 
         //     $non_unique_group_ID = $get_group_aa['group_ID'];
@@ -99,7 +107,7 @@
         }
     ?>
 
-    <a class="text-light fs-1" href="index.php?page=home" role="button">Swipe card</a>
+
 
   </body>
 </html>
--- master/sign_in.php
+++ master/sign_in.php
@@ -1,2 +1,15 @@
-<h2> You have signed in </h2>
-<a href="index.php">Click here to go back</a>
+<body style="background-color: rgb(0,65,122);">
+<div class="container d-flex align-items-center justify-content-center vstack text-light" >
+
+      <h1> You have signed in </h1>
+
+      <p>you will be redorected in 3 secods</p>
+
+</div>
+
+
+<?php
+  header('Refresh:3 ; URL=index.php?page=home');
+ ?>
+
+</body>
