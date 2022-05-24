a87d0be4a0a027c6635dd7a23566bc04e20b0b49
digstm0126 <78323032+digstm0126@users.noreply.github.com>
2022-05-25 10:28:21 +1200

    fixed the git hub pop in paints

--- master/home.php
+++ master/home.php
@@ -12,26 +12,21 @@
     <title>home</title>
   </head>
 
-  <body style="background-color: rgb(0,65,122);">
+  <body>
 
-<div class="container-fluid">
+<div class="container-fluid  d-flex align-items-center justify-content-center vstack text-light">
 
-  <div class="row">
-    <div class=" col-12 border d-flex align-items-center justify-content-center" style="height: 1080px; width: 1920px;">
-      <div class="row d-flex align-items-center justify-content-center">
 
-        <div class="col-12 border border-4 border-light rounded-1 d-flex align-items-center justify-content-center " style="height: 200px; width: 600px;">
+
           <p class="text-light fs-1" href="index.php?page=buttonns" role="button">Swipe Card</p>
-        </div>
 
-        <div class="col-12 ">
+
+
           <form action="index.php?page=reason" method="post">
             <div class="form-group">
                 <label for="student_ID"></label>
-                <input name="student_ID" type="number" autofocus="on" autocomplete="off" class="form-control border border-0 bg-success" onblur="this.focus()" style="--bs-bg-opacity: .0;">
-            </div>
-          </form>
-        </div>
+                <input name="student_ID" type="number" autofocus="on" autocomplete="off" class="form-control border border-0 bg-success " onblur="this.focus()" style=" color: rgb(0,65,122); --bs-bg-opacity: .0;">
+              </div>
 
       </div>
     </div>
@@ -41,5 +36,3 @@
 
   </body>
 </html>
-
-
---
+++ master/no_id.php
@@ -0,0 +1,16 @@
+<body>
+
+<div class="container d-flex align-items-center justify-content-center vstack text-light" >
+
+      <h1> no such id </h1>
+
+      <p>you will be redorected in 3 secods</p>
+
+</div>
+
+
+<?php
+  header('Refresh:3 ; URL=index.php?page=home');
+ ?>
+
+</body>
