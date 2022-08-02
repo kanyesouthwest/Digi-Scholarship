850e0594cdc2b2359e684afb2fe01c7b71b99197
digstm0126 <78323032+digstm0126@users.noreply.github.com>
2022-08-02 14:44:53 +1200

    fixed new addmein

--- master/newAdmin.php
+++ master/newAdmin.php
@@ -8,32 +8,45 @@
 </head>
 <body>
 
+
+
+    <!-- navbar -->
+    <nav class="navbar navbar-light sticky-top  border-bottom border-3" style="background-color: rgb(0, 49, 85);" >
+        
+        <div class="container-fluid">
+
+            <a class="navbar-brand" href="#">
+                <img src="photo/StACLogoWhite.png" alt="StACLogo" style="height: 40px; width: 60px;">
+            </a>
+
+            <div class="col d-lg-block d-none d-flex align-items-cente justify-content-center">
+
+                <a class="btn btn-primary roundedconners" href="export_daily.php" role="button">Export</a>
+
+            </div>
+
+            <!-- shurch bar -->
+            <div class="col d-flex align-items-cente justify-content-center ">
+                
+                <input type="text" placeholder="name">
+            
+            <!-- close shuch bar -->    
+            </div>
+
+            
+            <div class="col d-flex align-items-cente justify-content-end ">
+                <a class="btn btn-primary roundedconners" href="login.php" role="button">log out</a>
+            </div>
+        </div>
+
+    <!-- close navbar -->
+    </nav>
+
     <!-- container -->
     <div class="contaner-fluid">
-        <!-- navbar -->
-        <nav class="navbar navbar-light sticky-top " style="background-color: #e3f2fd;" >
+        
 
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    <img src="photo/StACLogoWhite.png" alt="StACLogo" style="height: 40px; width: 60px;">
-                </a>
-
-                <div class="col navbar-collapse d-none d-block-lg">
-                    Export
-                </div>
-                
-                <div class="col d-flex align-items-left justify-content-left"><a href="home.php"> logout </a></div>
-            </div>
-        <!-- close navbar -->
-        </nav>
-
-        <!-- shurch bar -->
-        <div class="">
-
-            such
-
-        <!-- close shuch bar -->
-        </div>
+         
 
         <!-- holds the col table -->
         <div class="row m-1">
